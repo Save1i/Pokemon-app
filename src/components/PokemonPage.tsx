7b180d5dfac0c3f8d8742af406459c6ac6f609a5
Save1i <savva0512@gmail.com>
2026-03-13 16:21:35 +0300

    give more info about pokemon, write request for geting a pokemon description

--- src/components/PokemonPage.tsx
+++ src/components/PokemonPage.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid"
 import { useEffect, useState } from "react"
 
 interface NamedAPIResource {
@@ -15,16 +16,37 @@ interface Pokemon {
   base_experience: number,
   is_default: boolean,
   abilities: PokemonAbility,
+}
 
+
+
+type FlavorText = {
+  flavor_text: string,
+  language: NamedAPIResource,
+  version: NamedAPIResource
+}
+
+interface PokemonSpecies {
+  id: number,
+  is_legendary: boolean,
+  name: string,
+  flavor_text_entries: Array<FlavorText>
 }
 
 const PokemonPage = () => {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null)
+  const [pokemonSpecies , setPokemonSpecies] = useState<PokemonSpecies | null>(null)
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
       .then((el) =>el.json())
       .then((el) => setPokemon(el))
+  }, [])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon-species/pikachu')
+      .then((el) =>el.json())
+      .then((el) => setPokemonSpecies(el))
   }, [])
 
   if(pokemon === null) {
@@ -37,6 +59,19 @@ const PokemonPage = () => {
       <div>
         <h1>{pokemon.name}</h1>
         <p>Base exp. {pokemon.base_experience}</p>
+        
+        <span>
+          {
+            pokemonSpecies ?
+              pokemonSpecies.flavor_text_entries.map((descrip) => {
+                if(descrip.language.name === 'en' && descrip.version.name === 'emerald') {
+                  return <li key={nanoid()}>{descrip.flavor_text}</li>
+                }
+              } )
+             : <p>...</p>
+          
+          }
+        </span>
       </div>
     </>
     
