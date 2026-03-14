4df8a0d4b93d5aba7b228588ddb7807a5ddffeb1
Save1i <savva0512@gmail.com>
2026-03-14 23:14:59 +0300

    add: strat do modal

--- src/components/PokemonDetailPage.tsx
+++ src/components/PokemonDetailPage.tsx
@@ -7,21 +7,27 @@ type Props = {
 
 const PokemonDetailPage = ({stats}: Props) => {
   return (
-    <div className="row g-3">
-      {
-        stats.map(pokemonStat => {
-          return (
-            <div className="col-6" key={nanoid()}>
-              <div className="stat_item ">
-                <p>{pokemonStat.stat.name}</p>
-                <div>
-                  <span>{pokemonStat.base_stat}</span>
+    <div className="pokemon_detail">
+      <div className="row g-3">
+        {
+          stats.map(pokemonStat => {
+            return (
+              <div className="col-6" key={nanoid()}>
+                <div className="stat_item ">
+                  <p>{pokemonStat.stat.name}</p>
+                  <div className="d-flex align-items-center gap-3">
+                    {pokemonStat.base_stat}
+                    <div className="progress flex-grow-1" role="progressbar" aria-label="Basic example" aria-valuenow={0} aria-valuemin={0} aria-valuemax={100} style={{height: "6px"}}>
+                      <div className="progress-bar" style={{width: `${pokemonStat.base_stat}%`}}></div>
+                    </div>
+                  </div>
+                  
+                  </div>
                 </div>
-              </div>
-            </div>
-          )
-        })
-      }
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
