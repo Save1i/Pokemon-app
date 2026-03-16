6caffd7c99dcfe4e82cc5c83c0b969b9b1d9d7d4
Save1i <savva0512@gmail.com>
2026-03-16 15:36:53 +0300

    create router routes

---
+++ src/appRouters/AppRouters.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router"
+import App from "../App"
+import PokemonPage from "../components/PokemonPage"
+import PokemonsList from "../components/PokemonsList"
+import Overview from "../overview/Overview"
+
+const AppRouters = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Overview />} />
+			<Route path='/pokemon-page' element={<PokemonPage />} />
+			<Route path='/pokemon-list' element={<PokemonsList />} />
+		</Routes>
+	)
+}
+
+export default AppRouters
