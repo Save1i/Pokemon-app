e51dcd4bd7ccab5d739a65b853bd66bbcfb7f583
Save1i <savva0512@gmail.com>
2026-03-17 10:36:24 +0300

    add Layout for router

--- src/appRouters/AppRouters.tsx
+++ src/appRouters/AppRouters.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router"
 import PokemonPage from "../components/PokemonPage"
 import PokemonsList from "../components/PokemonsList"
-import Overview from "../overview/Overview"
+import Layout from "../components/Layout"
+import App from "../App"
 
 const AppRouters = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Overview />} />
-			<Route path='/pokemon-page' element={<PokemonPage />} />
-			<Route path='/pokemon-list' element={<PokemonsList />} />
+			<Route element={<Layout />}>
+				<Route path='/' element={<App />} />
+				<Route path='/pokemon-page' element={<PokemonPage />} />
+				<Route path='/pokemon-list' element={<PokemonsList />} />
+			</Route>
 		</Routes>
 	)
 }
