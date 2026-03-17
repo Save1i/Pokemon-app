15111712a26734445800fda6034878eda6f51eb6
Save1i <savva0512@gmail.com>
2026-03-17 11:45:21 +0300

    chnage paths to make flexible paths

--- src/appRouters/AppRouters.tsx
+++ src/appRouters/AppRouters.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router"
 import PokemonPage from "../components/PokemonPage"
-import PokemonsList from "../components/PokemonsList"
 import Layout from "./Layout"
 import App from "../App"
+import Pokedex from "../components/Pokedex"
 
 const AppRouters = () => {
 	return (
 		<Routes>
 			<Route element={<Layout />}>
 				<Route path='/' element={<App />} />
-				<Route path='/pokemon-page' element={<PokemonPage />} />
-				<Route path='/pokemon-list' element={<PokemonsList />} />
+				<Route path='/pokemon-page/:pokemon' element={<PokemonPage />} />
+				<Route path='/pokedex' element={<Pokedex />} />
 			</Route>
 		</Routes>
 	)
