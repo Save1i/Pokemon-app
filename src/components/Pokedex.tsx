0f39f8d71dc9f2c5be3f88687dc8faa9c47c472b
Save1i <savva0512@gmail.com>
2026-03-18 11:01:33 +0300

    remove unused code

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -26,7 +26,6 @@ const Pokedex = () => {
     navigate(`/pokedex/${lastPokemon}`)
   }, [])
 
-	// Всегда загружаем список покемонов
 	useEffect(() => {
 		fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
 			.then(el => el.json())
@@ -39,40 +38,6 @@ const Pokedex = () => {
 
 	return (
 		<div>
-			{/* Показываем информацию о последнем покемоне, но не редиректим */}
-			{lastPokemon && (
-				<div
-					style={{
-						marginBottom: '20px',
-						padding: '10px',
-						background: '#f0f0f0',
-						borderRadius: '5px',
-					}}
-				>
-					<p>
-						Last viewed: <strong>{lastPokemon}</strong>
-					</p>
-					<NavLink
-						to={`/pokedex/${lastPokemon}`}
-						className='btn btn-primary'
-						style={{ marginRight: '10px' }}
-					>
-						Back to {lastPokemon}
-					</NavLink>
-
-					{/* Добавляем кнопку для сброса */}
-					<button
-						onClick={() => {
-							localStorage.removeItem('pokemon-storage')
-							window.location.reload()
-						}}
-						className='btn btn-secondary'
-					>
-						Show all pokemons
-					</button>
-				</div>
-			)}
-
 			<ul>
 				{pokemons.results.map(pokemon => (
 					<li key={nanoid()}>
