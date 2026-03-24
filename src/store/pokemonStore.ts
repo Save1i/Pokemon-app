d795df5dd263c1af2ca74d57c39c126927faad7d
Save1i <savva0512@gmail.com>
2026-03-24 10:55:12 +0300

    update name for myPokemons on playerPokemons

--- src/store/pokemonStore.ts
+++ src/store/pokemonStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-interface PokemonData {
+export interface PokemonData {
   id: number
   name: string
 }
@@ -11,7 +11,7 @@ interface PokemonStore {
 	setLastPokemon: (pokemon: string) => void
 	clearLastPokemon: () => void
 
-  myPokemons: PokemonData[],
+  playerPokemons: PokemonData[],
   addPokemon: (pokemon: PokemonData) => void
 }
 
@@ -19,21 +19,21 @@ export const usePokemonStore = create<PokemonStore>()(
 	persist(
 		set => ({
 			lastPokemon: null,
-      myPokemons: [],
+      playerPokemons: [],
 
 			setLastPokemon: pokemon => set({ lastPokemon: pokemon }),
 			clearLastPokemon: () => set({ lastPokemon: null }),
 
       addPokemon: (pokemon) => set((state) => {
 
-        const exist = state.myPokemons.some(el => el.id === pokemon.id)
+        const exist = state.playerPokemons.some(el => el.id === pokemon.id)
         if(exist) {
           console.log("you already have this pokemon")
           return state
         }
 
         return {
-          myPokemons: [...state.myPokemons, pokemon]
+          playerPokemons: [...state.playerPokemons, pokemon]
         }
 
       })
