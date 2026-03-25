5850d6b339d2e67d137fed33ef8361adef0ab48e
Save1i <savva0512@gmail.com>
2026-03-26 00:28:13 +0300

    add request for get pokemon data for player library

--- src/components/PlayerPokemonLibrary.tsx
+++ src/components/PlayerPokemonLibrary.tsx
@@ -1,16 +1,54 @@
+import { useEffect, useState } from "react"
 import { usePokemonStore } from "../store/pokemonStore"
+import type { Pokemon } from "./PokemonPage"
+import { nanoid } from "nanoid"
 
 const PlayerPokemonLibrary = () => {
 	const playerPokemons = usePokemonStore(state => state.playerPokemons)
+  const [pokemonData, setPokemonData] = useState<Pokemon[] | null>(null)
 	
 	if (playerPokemons.length === 0) {
 		return <p>You library is empty</p>
 	}
+
+  useEffect(() => {
+
+
+
+    const myPokemon = async() => {
+      try {
+        const playerLibrary = await Promise.all(playerPokemons.map( async (pokemon) => {
+          const detailResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}`)
+          const detailData = await detailResponse.json()
+
+          console.log(detailData)
+
+          return detailData
+        }))
+
+          setPokemonData(playerLibrary)
+
+      } catch(error) {
+        console.log(error)
+      }
+
+
+
+    }
+
+    
+      myPokemon()
+  }, [])
+
 		return (
 			<div>
-				{playerPokemons.map(el => (
-					<p>{el.name}</p>
-				))}
+			{
+        pokemonData ?
+        pokemonData.map(el => (
+					<p key={nanoid()}>{el.name}</p>
+				)) : <p>nothing</p>
+      
+      }
 			</div>
 		)
 }
