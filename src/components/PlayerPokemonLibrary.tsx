4ed7b594ae1b6e095b85f5921d678e2ce663837c
Save1i <savva0512@gmail.com>
2026-04-20 12:32:47 +0300

    update: add usePlayerPokemon, make ui layer thinner, remove fetching logic and states

--- src/components/PlayerPokemonLibrary.tsx
+++ src/components/PlayerPokemonLibrary.tsx
@@ -1,57 +1,17 @@
 // PlayerPokemonLibrary.tsx
-import { useEffect, useState, useCallback } from 'react'
+import { useCallback } from 'react'
 import { usePokemonStore } from '../store/pokemonStore'
-import { getPokemons } from '../Api/getPokemons'
+import { usePlayerPokemons } from '../hooks/usePlayerPokemons'
 
-interface Pokemon {
+export interface Pokemon {
   id: number
   name: string
   image: string
 }
 
 const PlayerPokemonLibrary = () => {
-  const playerPokemonsName = usePokemonStore(state => state.playerPokemons) as { id: number; name: string }[]
   const { removeAllPlayserPokemon } = usePokemonStore()
-
-  const [loading, setLoading] = useState(false)
-  const [playerPokemons, setPlayerPokemons] = useState<Pokemon[] | null>(null)
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    if (!playerPokemonsName?.length) {
-      setPlayerPokemons(null)
-      return
-    }
-
-    let isMounted = true // Предотвращаем обновления после размонтирования
-
-    const fetchPokemon = async () => {
-      setLoading(true)
-      setError(null)
-
-      try {
-        const pokemonData = await getPokemons(playerPokemonsName)
-        
-        if (isMounted) {
-          setPlayerPokemons(pokemonData)
-        }
-      } catch (error) {
-        if (isMounted) {
-          setError(error instanceof Error ? error.message : 'Unknown error')
-        }
-      } finally {
-        if (isMounted) {
-          setLoading(false)
-        }
-      }
-    }
-
-    fetchPokemon()
-    
-    return () => {
-      isMounted = false
-    }
-  }, [playerPokemonsName])
+  const {playerPokemons, loading, isEmpty} = usePlayerPokemons()
 
   const removeAllPokemons = useCallback(() => {
     if (confirm("Are you sure?")) {
@@ -59,14 +19,12 @@ const PlayerPokemonLibrary = () => {
     }
   }, [removeAllPlayserPokemon])
 
-  // Ранние возвраты
   if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error}</p>
-  if (!playerPokemons?.length) return <p>Your library is empty</p>
+  if (isEmpty) return <p>Your library is empty</p>
 
   return (
     <div>
-      {playerPokemons.map(pokemon => (
+      {playerPokemons?.map(pokemon => (
         <div key={pokemon.id}>
           <img src={pokemon.image} alt={pokemon.name} />
           <p>{pokemon.name}</p>
